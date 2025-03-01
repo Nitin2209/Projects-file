0ffcb764bb1d83cf92c141126cd8f0c3acedb2dc
Nitin2209 <nitnjagtap979gmail.com>
2025-03-01 16:03:12 +0530

    remove previous commits

---
+++ GameMain.java/Main.java
@@ -0,0 +1,101 @@
+
+// import java.util.Random;
+// import java.util.Scanner;
+// public class Main {
+//     public static void main(String[] args) {
+         
+//         //  int rocke = 0
+//         //  int Paper = 1
+//         //  int Scissor = 2
+//             Scanner sc = new Scanner(System.in);
+//             int userinput = sc.nextInt();
+
+//             Random random = new Random();
+//              int computerinput = random.nextInt( 3);
+
+//              if (computerinput==userinput) {
+//                System.out.println("drow or no one wins or no one lose");
+              
+//              }
+//              else if (computerinput==0&& userinput==1 || computerinput==1&& userinput==2 || computerinput==2&& userinput==0) {
+//               System.out.println("Human wins");
+              
+//              }
+//              else {
+//               System.out.println("computerinput wins");
+//              }
+//              System.out.println("computer choice is" + computerinput);
+//              if (computerinput==0) {
+//               System.out.println("computer choice is rocke");
+//              }
+//              else if (computerinput==1) {
+//               System.out.println("computer choice is paper");
+//              }
+//               else{
+//                 System.out.println("computer choice is Scissor");
+//               }
+               
+//             }
+        
+//           }
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    
+      public static void main(String[] args) {
+         int rocke = 0;
+         int Paper = 1;
+         int Scissor = 2;
+         
+        Scanner sc = new Scanner(System.in);
+        int user = sc.nextInt();
+
+        Random rnd = new Random();
+        int computer = rnd.nextInt(3);
+        
+        if (user==0) {
+          System.out.println("Human choice rocke");
+        }
+        else if (user==1) {
+          System.out.println("Human choice paper");
+        }
+        else if(user==2){
+          System.out.println("Human choice scissor");
+        }
+        else if(user<0){
+          System.out.println("invalid choice \n choose only under 0,1,2");
+        }
+        else if (user>2) {
+          System.out.println("invalid choice \n choose only under 0,1,2");
+        }
+        if (computer==0) {
+          System.out.println("computer choice is rocke");          
+        }
+        else if (computer==1) {
+          System.out.println("computer choice is paper");
+        }
+        else if (computer==2) {                                                                     
+          System.out.println("computer choice is scissor");
+        }
+        if (computer==user) {
+          System.out.println("Drow or no one wins or no one lose");
+        }
+        else if (computer==0&& user==1 || computer==1&& user==2 || computer==2&& user==0 ) {
+          System.out.println("Human wins");
+        }
+        else if (computer==1&& user==0 || computer==2&& user==1 || computer==0&& user==2) {
+          System.out.println("computer wins");
+        }
+       if (user<0) {
+          System.out.println("??");
+        } 
+        else if (user>2) {
+          System.out.println("??");
+        }
+
+      }
+    }
+    
+
---
+++ GameMain.java/tempCodeRunnerFile.java
@@ -0,0 +1,2 @@
+    if (computer==user) {
+          System.out.println("Drow or no one wins or no one lose");
